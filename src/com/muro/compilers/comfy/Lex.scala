8c667bf038065c1c22086885c94fc355107fc6ff
JM <jmuro0321@gmail.com>
2014-02-23 22:10:52 -0500

    Still experimenting with the lexer. As of right now, Lex performs two passes on the source input: On the first pass it processes the source line-by-line, trimming leading and trailing whitespace, and reducing sequences of multiple white space chars to a single blank; The second pass will process the source one character at a time using the "consume" function. Created a Terminal class containing members corresponding to each terminal in the grammar.

--- src/com/muro/compilers/comfy/Lex.scala
+++ src/com/muro/compilers/comfy/Lex.scala
@@ -7,54 +7,82 @@
 package com.muro.compilers.comfy
 
 import scala.io._
-
 import java.io._
 
 object Lex {
 
+  /**
+   * Contains the tokens extracted from the source input.
+   */
   val tokens = new scala.collection.mutable.Queue[Token]
+  
+  /**
+   * Contains the characters awaiting lexical analysis.
+   */
   val buffer = new scala.collection.mutable.ArrayBuffer[Char]
   
-  val LeftBrace = '{'
-  val RightBrace = '}'
-  val LeftParen = '('
-  val RightParen = ')'
-  val DoubleQuote = '"'
-  val EndOfProgram = '$'
+  var nLetters: Int = 0
+  var nSpaces: Int = 0
+  var nNewlines: Int = 0
+  var nPunctuation: Int = 0
+  var nDigits = 0
+  var nUnclassified = 0
   
   def tokenize(f: String): Unit = {
+    
+    val source: StringBuilder = new StringBuilder()
+    
     Source.fromFile(new File(f)).getLines.foreach { line =>
-      val charList: Array[Char] = line.toCharArray
-      for (c <- charList) {
-        println(c)
-        c.isDigit
-      }
+      // Strip leading and trailing whitespace and reduce extraneous whitespace.
+      source.append(line.trim().replaceAll("(\\s)(2,}", " ") + "\n")
     }
+    
+    val i: Int = 0
+    
+    if (source.charAt(0) != '{') {
+      // Insert a left brace and issue a warning.
+    }
+    
+    if (source.charAt(source.length-1) != '$') {
+      // Insert the end-of-program delimiter and issue a warning.
+    }
+    
+    Source.fromIterable(source).foreach { c =>
+      consume(c)
+    }
+    
+    def consume(c: Char): Unit = {
+    if (c.isDigit) 
+      nDigits += 1
+    else if (c.isLetter)
+      nLetters += 1
+    else if (c.isSpaceChar)
+      nSpaces += 1
+    else if (c =='\n')
+      nNewlines += 1
+    else
+      System.out.print(" " + c)
+  }
+    
+    System.out.println("\nLetters: " + nLetters)
+    System.out.println("Digits: " + nDigits)
+    System.out.println("Newlines: " + nNewlines)
+    System.out.println("Spaces: " + nSpaces)
+    System.out.println("Unclassified: " + nUnclassified)
   }
 
-  def consume(c: Char): Char = {
-    if (c == RightParen) {
-      c
-    } else if (c.isLetter) {
-      c
-    } else if () {
-      c
-    } else if (c == LeftParen) {
-      c
-    }
-  }
-
-  def nextToken(): Token = {
-    new Token(Tag._boolOp, "")
+  def nextToken(): Token = {  
+    if (hasNextToken())
+      tokens.dequeue
+    else
+      null
   }
   
   def hasNextToken(): Boolean = {
     !tokens.isEmpty
   }
-
+  
   def main(args: Array[String]): Unit = {
     tokenize(args(0))
   }
-  
 }
-
---
+++ src/com/muro/compilers/comfy/Terminal.scala
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.muro.compilers.comfy
+
+object Terminal {
+  val LeftBrace = "{"
+  val RightBrace = "}"
+  val LeftParen = "("
+  val RightParen = ")"
+  val DoubleQuote = "\""
+  val EndOfProgram = "$"
+  val If = "if"
+  val Print = "print"
+  val While = "while"
+  val Int = "int"
+  val String = "string"
+  val Boolean = "boolean"
+  val EqualsOp = "=="
+  val NotEqualsOp = "!="
+  val BoolFalse = "false"
+  val BoolTrue = "true"
+  val PlusOp = "+"
+  val Space = " "
+}
